c048fdfdacbc2b89df46c40a05bd7b4866d98b88
Alaina Garcia Salazar <alaina_garcia@ucne.edu.do>
2025-07-17 22:24:17 -0400

    Modelo y Dto de Vehiculos realizado

---
+++ Usuarios.Domain/DTO/VehiculosDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usuarios.Domain.DTO;
+
+public class VehiculosDto
+{
+	public int VehiculoId { get; set; }
+	public string? Descripcion { get; set; }
+	public double Precio { get; set; }
+}
